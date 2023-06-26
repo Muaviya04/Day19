a45f591c3314b9ea0d5a826b80c2dfc70fb9c950
Muaviya04 <muaviyamomin46@gmail.com>
2023-06-26 19:11:38 +0530

    Added

---
+++ AddressBookMain1.java
@@ -0,0 +1,9 @@
+public class AddressBookMain1 {
+    public static void main(String[] args) {
+        // Create a new contact
+        Contact contact = new Contact("John", "Doe", "123 Main St", "City", "State", "12345", "1234567890", "johndoe@example.com");
+
+        // Display the contact details
+        System.out.println(contact);
+    }
+}
